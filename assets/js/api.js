969b238e2857ad35ff4580f30cef7dc6b14720ed
Charlotte Mountain <charr.mt@gmail.com>
2020-03-25 16:40:28 -0700

    EDAMAM added

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,4 +1,3 @@
-
 // -------------------------THE MET API---------------------------------
 // var region1 = ["america", "europe", "africa", "asia"]
 // var region2 = ["america", "europe", "africa", "asia"]
@@ -8,10 +7,10 @@
       var region2 = "Europe"
       var queryURL1 = "https://collectionapi.metmuseum.org/public/collection/v1/search?geoLocation="+region1+"&q=statue&medium=Sculpture&hasImages=true";
       var queryURL2 = "https://collectionapi.metmuseum.org/public/collection/v1/search?geoLocation="+region2+"&q=statue&medium=Sculpture&hasImages=true";
-      
+
       console.log(region1)
       console.log(region2)
-
+//ART SPOT #1
     $.ajax({
         url: queryURL1,
         method: "GET"
@@ -20,10 +19,11 @@
         var objectIDArray1 = response1.objectIDs;
         var randObjectID1 = objectIDArray1[Math.floor(Math.random() * objectIDArray1.length)];
         console.log(randObjectID1)
-        //out of random objects fitting criteria
+        //out of random objects fitting criteria call function 
         renderArt1(randObjectID1);
-      });
+        });
 
+//ART SPOT #2
       $.ajax({
         url: queryURL2,
         method: "GET"
@@ -32,32 +32,30 @@
         var objectIDArray2 = response2.objectIDs
         var randObjectID2 = objectIDArray2[Math.floor(Math.random() * objectIDArray2.length)]
         console.log(randObjectID2)
-        //out of random objects fitting criteria
+        //out of random objects fitting criteria call function
         renderArt2(randObjectID2);
       });
 
-    // get art from objectID
+    // get art from objectID ART #1
     function renderArt1 (randObjectID) {
         //UV place UFL
         var queryURL = "https://collectionapi.metmuseum.org/public/collection/v1/objects/"+randObjectID
         
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-            })
-              .then(function (response) {
-                  console.log(response)
+      $.ajax({
+        url: queryURL,
+        method: "GET"
+      }).then(function (response) {
+        console.log(response)
 
-                  var imageUrl=response.primaryImageSmall
-                  var artist =response.artistDisplayName
-                  var title = response.title
+        var imageUrl=response.primaryImageSmall
+        var artist =response.artistDisplayName
+        var title = response.title
                 
-                  var date = response.objectDate
-                  var medium = response.medium
-                  var country = response.country
-                  var dimensions = response.dimensions
-                  // var metLink = response. 
-
+        var date = response.objectDate
+        var medium = response.medium
+        var country = response.country
+        var dimensions = response.dimensions
+        // var metLink = respon
                 if (artist=== ""){
                   artist.text
                   $("#artist1").text("Unknown")
@@ -84,19 +82,18 @@
                   $("#title1").append(title)
                   $("#region1").append(country)
                   $("#date1").append(date)
-                  $("#medium1").append(medium)
-                  $("#dimensions1").append(dimensions)
+                                  $("#dimensions1").append(dimensions)
                 }
               });
         };
 
-        // get art from objectID
-        function renderArt2 (randObjectID) {
+    // get art from objectID ART #2
+    function renderArt2 (randObjectID) {
             //UV place UFL
-            var queryURL = "https://collectionapi.metmuseum.org/public/collection/v1/objects/"+randObjectID
+        var queryURL = "https://collectionapi.metmuseum.org/public/collection/v1/objects/"+randObjectID
             
-            $.ajax({
-                url: queryURL,
+        $.ajax({
+            url: queryURL,
                 method: "GET"
                 })
                   .then(function (response) {
@@ -145,4 +142,42 @@
             };
   // });
 
-  // -------------------------EDAMAM API---------------------------------
+
+// -------------------------EDAMAM API---------------------------------
+
+// $("a").on("click", function() {
+
+  var region1 = "asian"
+  var APIid = "1da6de4c";
+  var APIKey = "b4525aef1893921308c30d0194460591";
+  var queryURL = "https://api.edamam.com/search?q="+region1+"&app_id="+APIid+"&app_key="+APIKey;
+
+//recipe from the region you were in
+for (var i = 0; i <3;i++){
+$.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function(response) {
+    console.log(response);
+    var recipeArray = response.hits;
+    var randRecipe = recipeArray[Math.floor(Math.random() * recipeArray.length)];
+    console.log(randRecipe)
+    // out of random objects fitting criteria call function 
+    renderRecipe(randRecipe);
+    });
+
+  function renderRecipe(randRecipe){
+      //get recipes title, image, and link
+      var recipeTitle = $("<p>").text(randRecipe.recipe.label);
+      var recipeImage = $("<img>").attr('src',randRecipe.recipe.image);
+      var recipeHealth= $("</p>").text(randRecipe.recipe.healthLabels);
+      var recipeURL= $("<p>").text(randRecipe.recipe.url);
+      $(".recipe").append(recipeTitle); 
+      $(".recipe").append(recipeImage);
+      $(".recipe").append(recipeURL);
+      $(".recipe").append(recipeHealth);
+
+    };
+  }
+
+// });

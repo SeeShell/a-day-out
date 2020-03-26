9b0a3e6af719a50603cebfd6df4f5163f3b9ba0a
See Shell <323seeshell@gmail.com>
2020-03-26 07:54:26 -0700

    render regions functions added

--- assets/js/script.js
+++ assets/js/script.js
@@ -55,9 +55,12 @@ $("button").on("click", function() {
 })
 
 // Adds user choice region to text area
-
 function renderRegions(regionName){
-    
+    if ($("#first-region").text = ""){
+        $("#first-region").text(regionName);
+    }else{
+        $("#second-region").text(regionName);
+    }
 }
 
 //---------- Projects Page functions ------------------------------

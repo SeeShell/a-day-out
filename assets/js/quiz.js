3bc0ba9a6ba1bdc4187984a70b130f314e02f8a0
Charlotte Mountain <charr.mt@gmail.com>
2020-03-28 14:03:01 -0700

    cards added

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,7 +1,7 @@
 var objectIDsArray = JSON.parse(localStorage.getItem("objectIDsArray"));
-console.log(objectIDsArray)
+
 var randObjectID = objectIDsArray[Math.floor(Math.random() * objectIDsArray.length)];
-console.log(randObjectID)
+
 renderQuizImage(randObjectID);
 
 function renderQuizImage (randObjectID) {
@@ -15,19 +15,44 @@ function renderQuizImage (randObjectID) {
       }).then(function(response) {
  
         var imageUrl = response.primaryImageSmall;
-      
         var quizImage = $("<img>").attr("src", imageUrl);
         $("#flash-card").append(quizImage);
-
-
-        // var artist = response.artistDisplayName;
-        // var title = response.title;
   
-        // var date = response.objectDate;
-        // var medium = response.medium;
-        // var country = response.country;
-        // var dimensions = response.dimensions;
-        // var metLink = response.objectURL;
+        var artist = response.artistDisplayName;
+        var title = response.title;
+  
+        var date = response.objectDate;
+        var medium = response.medium;
+        var country = response.country;
+        var dimensions = response.dimensions;
+
+        var titleCard = $("<h2>").text(title); 
+        var artistCard = $("<p>").text("Artist: " + artist);
+        var dateCard = $("<p>").text("Date: " + date);
+        var mediumCard = $("<p>").text("Medium: " + medium);
+        var countryCard = $("<p>").text("Country: " + country);
+        var dimensionsCard =$("<p>").text("Dimensions: " + dimensions);
+
+        if (title === "") {
+          var titleCard = $("<h2>").text("Unknown");
+        }
+        if (artist === "") {
+          var artistCard = $("<p>").text("Artist: Unknown");
+        }
+        if (date === "") {
+          var dateCard = $("<p>").text("Date: Unknown");
+        }
+        if (medium === "") {
+          var mediumCard = $("<p>").text("Medium: Unknown");
+        }
+        if (country === "") {
+          var countryCard = $("<p>").text("Country: Unknown");
+        }
+        if (dimensions === "") {
+          var dimensionsCard =$("<p>").text("Country: Unknown");
+        } 
+
+        $(".uk-text-right").append(titleCard, artistCard, dateCard, mediumCard, countryCard, dimensionsCard); 
 
     });
 }
